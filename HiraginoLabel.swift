11b3b1c4f35d5c5b8a6a41e59f5f5ea4e4735f7a
kohei1218 <yandapanda1218@gmail.com>
2019-03-07 19:32:35 +0900

    feat: HiraginoLabel and example

---
+++ HiraginoLabel.swift
@@ -0,0 +1,45 @@
+//
+//  HiraginoLabel.swift
+//  HiraginoLabel
+//
+//  Created by kohei saito on 2019/03/07.
+//
+
+import UIKit
+
+public class HiraginoLabel: UILabel {
+    
+    override public var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        size.height += ceilMargin * 2
+        return size
+    }
+    
+    override public func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, UIEdgeInsets(top: ceilMargin,
+                                                                    left: 0.0,
+                                                                    bottom: ceilMargin,
+                                                                    right: 0.0)))
+    }
+    
+    override public var alignmentRectInsets: UIEdgeInsets {
+        let alignmentRectInsets = super.alignmentRectInsets
+        return UIEdgeInsets(top: alignmentRectInsets.top + ceilMargin,
+                            left: alignmentRectInsets.left,
+                            bottom: alignmentRectInsets.bottom + ceilMargin,
+                            right: alignmentRectInsets.right)
+    }
+    
+    override public func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        size.height += ceilMargin * 2
+        return size
+    }
+    
+    private var ceilMargin: CGFloat {
+        guard let font = self.font else {
+            return 4.0
+        }
+        return ceil(font.pointSize / 6.0)
+    }
+}
